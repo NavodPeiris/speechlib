79eff0eeca548f02d687bf45ddafbc46c4358699
Killiane Letellier <killiane.letellier@mailo.com>
2024-05-11 09:01:48 +0200

    Add MPS fallback

--- speechlib/core_analysis.py
+++ speechlib/core_analysis.py
@@ -38,6 +38,7 @@ def core_analysis(file_name, voices_folder, log_folder, language, modelSize, qua
         device = torch.device("cuda")
     elif torch.backends.mps.is_available():
         device = torch.device("mps")
+        os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
     else:
         device = torch.device("cpu")
 
