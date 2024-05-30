f38c9689999f2d20c2580105af8da0e20200cadb
Tomas Caram <tomas.caram@gmail.com>
2024-05-30 10:19:11 -0300

    Initialize preprocessor in example befora calling it

--- examples/preprocess.py
+++ examples/preprocess.py
@@ -1,12 +1,13 @@
 from speechlib import PreProcessor
 
 file = "obama1.mp3"
-
+#initialize
+prep = PreProcessor()
 # convert mp3 to wav
-wav_file = PreProcessor.convert_to_wav(file)   
+wav_file = prep.convert_to_wav(file)   
 
 # convert wav file from stereo to mono
-PreProcessor.convert_to_mono(wav_file)
+prep.convert_to_mono(wav_file)
 
 # re-encode wav file to have 16-bit PCM encoding
-PreProcessor.re_encode(wav_file)
+prep.re_encode(wav_file)
