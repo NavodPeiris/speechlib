f24ff24e92bc42264a445c53635b152621d8c111
Navodplayer1 <navodpeiris1234@gmail.com>
2024-08-16 21:27:14 +0530

    fixed error

--- examples/transcribe.py
+++ examples/transcribe.py
@@ -2,7 +2,7 @@ from speechlib import Transcriptor
 
 file = "obama_zach.wav"  # your audio file
 voices_folder = "voices" # voices folder containing voice samples for recognition
-language = "zh"          # language code
+language = "en"          # language code
 log_folder = "logs"      # log folder for storing transcripts
 modelSize = "tiny"     # size of model to be used [tiny, small, medium, large-v1, large-v2, large-v3]
 quantization = False   # setting this 'True' may speed up the process but lower the accuracy
