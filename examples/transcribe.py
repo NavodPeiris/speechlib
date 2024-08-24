4d1cdd2ec7c63b22a6e6155beb405bf573c70e36
Navodplayer1 <navodpeiris1234@gmail.com>
2024-08-25 00:37:33 +0530

    fixed

--- examples/transcribe.py
+++ examples/transcribe.py
@@ -6,7 +6,7 @@ language = "el"          # language code
 log_folder = "logs"      # log folder for storing transcripts
 modelSize = "tiny"     # size of model to be used [tiny, small, medium, large-v1, large-v2, large-v3]
 quantization = False   # setting this 'True' may speed up the process but lower the accuracy
-ACCESS_TOKEN = "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" # get permission to access pyannote/speaker-diarization@2.1 on huggingface
+ACCESS_TOKEN = "your hf key" # get permission to access pyannote/speaker-diarization@2.1 on huggingface
 
 # quantization only works on faster-whisper
 transcriptor = Transcriptor(file, log_folder, language, modelSize, ACCESS_TOKEN, voices_folder, quantization)
@@ -24,4 +24,4 @@ transcriptor = Transcriptor(file, log_folder, language, modelSize, ACCESS_TOKEN,
 #res = transcriptor.huggingface_model("Jingmiao/whisper-small-chinese_base")
 
 # use assembly ai model
-res = transcriptor.assemby_ai_model("a114db239a5742f39172d0f2ac3d8cf0")
+res = transcriptor.assemby_ai_model("your api key")
