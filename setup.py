7eb565dbab25cbccfc8f6bca7e15854f91ef68aa
Navodplayer1 <navodpeiris1234@gmail.com>
2024-10-10 00:15:37 +0530

    changed requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("library.md", "r") as f:
 
 setup(
     name="speechlib",
-    version="1.1.9",  
+    version="1.1.10",  
     description="speechlib is a library that can do speaker diarization, transcription and speaker recognition on an audio file to create transcripts with actual speaker names. This library also contain audio preprocessor functions.",
     packages=find_packages(),
     long_description=long_description,
@@ -19,7 +19,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    install_requires=["transformers", "torch", "torchaudio", "pydub", "pyannote.audio", "speechbrain==0.5.16", "accelerate", "faster-whisper", "openai-whisper", "assemblyai"],
+    install_requires=["transformers>=4.36.2, <5.0.0", "torch>=2.1.2, <3.0.0", "torchaudio>=2.1.2, <3.0.0", "pydub>=0.25.1, <1.0.0", "pyannote.audio>=3.1.1, <4.0.0", "speechbrain>=0.5.16, <1.0.0", "accelerate>=0.26.1, <1.0.0", "faster-whisper>=0.10.1, <1.0.0", "openai-whisper>=20231117, <20240927", "assemblyai"],
     python_requires=">=3.8",
 )
 
