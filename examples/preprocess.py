3aa867d6fdb63a55ffeb94361833cd8ec5ab4f78
avillena <327649+avillena@users.noreply.github.com>
2026-03-08 21:11:42 -0300

    fix: examples/preprocess.py propagates paths through chain

--- examples/preprocess.py
+++ examples/preprocess.py
@@ -1,13 +1,10 @@
 from speechlib import PreProcessor
 
 file = "obama1.mp3"
-#initialize
 prep = PreProcessor()
-# convert mp3 to wav
-wav_file = prep.convert_to_wav(file)   
 
-# convert wav file from stereo to mono
-prep.convert_to_mono(wav_file)
+wav_file = prep.convert_to_wav(file)
+mono_file = prep.convert_to_mono(wav_file)
+enc_file = prep.re_encode(mono_file)
 
-# re-encode wav file to have 16-bit PCM encoding
-prep.re_encode(wav_file)
+print(f"Final processed file: {enc_file}")
